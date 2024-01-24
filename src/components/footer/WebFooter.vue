3977c382c6ecc8116800115def1dae66d4dbe144
wendikardian <wendikardian@gmail.com>
2024-01-24 12:50:44 +0700

    profile router fixed

--- src/components/footer/WebFooter.vue
+++ src/components/footer/WebFooter.vue
@@ -1,5 +1,5 @@
 <template>
-<footer class="container-fluid py-5 mt-5 text-white" style="background-color: var(--base-color);">
+    <footer class="container-fluid py-5 mt-5 text-white" style="background-color: var(--base-color);">
         <div class="container-md">
             <div class="row">
                 <div class="col-lg-3">
@@ -39,8 +39,44 @@
                     </ul>
                 </div>
             </div>
+            <div class="d-flex space-between social-media">
+                <div class="social-media-icons">
+                    <a href="your-whatsapp-link"><i class="fab fa-whatsapp"></i></a>
+                    <a href="your-instagram-link"><i class="fab fa-instagram"></i></a>
+                    <a href="your-linkedin-link"><i class="fab fa-linkedin"></i></a>
+                </div>
+                <div class="copyright-notice">
+                    Copyright &copy; Sharpie 2024
+                </div>
+            </div>
         </div>
 
     </footer>
-
 </template>
+
+
+<style>
+.d-flex {
+  display: flex;
+  justify-content: space-between;
+}
+
+.social-media{
+    margin-top: 20px;
+    padding: 7px;
+    border-top: 2px solid grey;
+}
+
+.social-media-icons a {
+  margin-right: 10px;
+
+}
+
+
+.social-media-icons i{
+    color: white;
+}
+.social-media-icons a:last-child {
+  margin-right: 0;
+}
+</style>
---
+++ src/components/user/UserMenu.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="col-12 col-lg-3">
+        <div class="row mt-4">
+            <h2>Settings</h2>
+        </div>
+        <div class="row mt-4" @click="menuClicked('personal-info')">
+            <a href="">Profile Detail</a>
+        </div>
+        <div class="row mt-4" @click="menuClicked('change-password')">
+            <a href="">Change Password</a>
+        </div>
+        <div class="row mt-4" @click="menuClicked('transaction-history')">
+            <a href="">Transaction History</a>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+const emit = defineEmits(['changeComponent']);
+
+
+const menuClicked = option => {
+    emit('changeComponent', option);
+}
+</script>
